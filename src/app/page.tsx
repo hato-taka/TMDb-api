1c907fd100313fd455d600ff1f78ff7db7e8d0e8
hato-taka <n28hashimoto@gmail.com>
2025-01-24 05:15:43 +0900

    FIX: ESlint の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Item } from "./components/Item";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Home() {
   const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY; // .env.local ファイルに TMDB API キーを保存
@@ -41,20 +41,30 @@ export default function Home() {
       })
       .catch((error) => {
         // エラー発生時の処理
-        console.log("error");
+        console.log(error);
       });
   }, []);
 
   return (
     <div>
       <ul className="flex flex-wrap w-[720px] mx-auto my-5 max-w-full">
-        {movieInfo.map((movie) => {
-          return (
-            <li key={movie.id} className="w-1/3 p-2">
-              <Item titleName={movie.title} imgPath={movie.poster_path} />
-            </li>
-          );
-        })}
+        <InfiniteScroll
+          dataLength={movieInfo.length} // 現在のアイテム数
+          next={fetchMoreData} // 次のデータをロードする関数
+          hasMore={hasMore} // まだデータがあるかどうか
+          loader={<h4>Loading...</h4>} // ローディング中に表示される内容
+          endMessage={
+            <p className="text-center my-4">You have seen all the items!</p>
+          } // 終了メッセージ
+        >
+          {movieInfo.map((movie) => {
+            return (
+              <li key={movie.id} className="w-1/3 p-2">
+                <Item titleName={movie.title} imgPath={movie.poster_path} />
+              </li>
+            );
+          })}
+        </InfiniteScroll>
       </ul>
     </div>
   );
