dac95f7643c45e17a5526c11f8e0014c25b03142
hato-taka <n28hashimoto@gmail.com>
2025-02-14 18:37:59 +0900

    DELETE: Movieテーブルを削除し、WishListテーブルを作成

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
-// import prisma from "@/app/lib/prisma";
+import prisma from "@/app/lib/prisma";
 
 // GETリクエストの処理（すべての movie を取得）
 export async function GET() {
   try {
-    // const movies = await prisma.movie.findMany();
-    // return NextResponse.json(movies, { status: 200 });
-    return NextResponse.json({movies: "test"}, { status: 200 });
+    const movies = await prisma.wishList.findMany();
+    return NextResponse.json(movies, { status: 200 });
+    // return NextResponse.json({movies: "test"}, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: `Failed to fetch movies. ${error}` }, { status: 500 });
   }
