9b7a56c756434cfdbcd4b98f2478aca77a3cd349
hato-taka <n28hashimoto@gmail.com>
2025-03-08 18:18:40 +0900

    FEAT: ActorおよびWishListテーブルの作成時にIF NOT EXISTSを追加

--- prisma/migrations/20250308082804_reset/migration.sql
+++ prisma/migrations/20250308082804_reset/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Actor` (
+CREATE TABLE IF NOT EXISTS `Actor`  (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
 
@@ -7,7 +7,7 @@ CREATE TABLE `Actor` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `WishList` (
+CREATE TABLE IF NOT EXISTS `WishList` (
     `id` VARCHAR(191) NOT NULL,
     `movieId` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
