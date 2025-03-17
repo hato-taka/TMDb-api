171e5662eaf0f468398268dd9771f40dfcdb6063
hato-taka <n28hashimoto@gmail.com>
2025-03-17 20:01:24 +0900

    FEAT: GETリクエストの処理をコメントアウトし、エラーハンドリングを一時的に無効化

--- src/app/api/movies/[id]/route.ts
+++ src/app/api/movies/[id]/route.ts
@@ -1,34 +1,35 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
+// TODO: エラーが出たため、コメントアウト
 // GETリクエストの処理（指定されたIDの映画を取得）
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
-  ) {
-    const { id } = params;
+// export async function GET(
+//     request: Request,
+//     { params }: { params: { id: string } }
+//   ) {
+//     const { id } = params;
   
-    if (!id) {
-      return NextResponse.json({ error: "ID is required" }, { status: 400 });
-    }
+//     if (!id) {
+//       return NextResponse.json({ error: "ID is required" }, { status: 400 });
+//     }
   
-    try {
-      const movie = await prisma.wishList.findUnique({
-        where: { id },
-      });
+//     try {
+//       const movie = await prisma.wishList.findUnique({
+//         where: { id },
+//       });
   
-      if (!movie) {
-        return NextResponse.json({ error: "Movie not found" }, { status: 404 });
-      }
+//       if (!movie) {
+//         return NextResponse.json({ error: "Movie not found" }, { status: 404 });
+//       }
   
-      return NextResponse.json(movie, { status: 200 });
-    } catch (error) {
-      return NextResponse.json(
-        { error: `Failed to fetch movie. ${error}` },
-        { status: 500 }
-      );
-    }
-  }
+//       return NextResponse.json(movie, { status: 200 });
+//     } catch (error) {
+//       return NextResponse.json(
+//         { error: `Failed to fetch movie. ${error}` },
+//         { status: 500 }
+//       );
+//     }
+//   }
 
 // DELETEリクエストの処理（指定された movie を削除）
 export async function DELETE(
