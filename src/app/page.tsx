d708d4aa75aa3dd5fb328618e953dc795f43bf72
hato-taka <n28hashimoto@gmail.com>
2025-04-13 20:22:39 +0900

    FEAT: InfiniteScrollコンポーネントを削除し、映画リストの表示を簡素化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { Item } from "./components/Item";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { useMovieList } from "./hooks/useMovieList";
 import { useMovie } from "./hooks/useMovie";
 import SearchForm from "./components/SearchForm";
 
 export default function Home() {
-  const { movieList, hasMore, fetchMoreData } = useMovieList();
+  const { movieList } = useMovieList();
   // TODO: isLoading の状態を追加する
   const { movieInfoList } = useMovie();
   // likes の降順で並び替え
@@ -38,15 +37,6 @@ export default function Home() {
 
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
       <h1 className="text-2xl font-bold mt-10">上映中作品</h1>
-      <InfiniteScroll
-        dataLength={movieList.length} // 現在のアイテム数
-        next={fetchMoreData} // 次のデータをロードする関数
-        hasMore={hasMore} // まだデータがあるかどうか
-        loader={<h4>Loading...</h4>} // ローディング中に表示される内容
-        endMessage={
-          <p className="text-center my-4">You have seen all the items!</p>
-        } // 終了メッセージ
-      >
         <div className="grid grid-cols-1 gap-4 mt-4">
           {movieList.map((movie) => (
             <Item
@@ -56,7 +46,6 @@ export default function Home() {
             />
           ))}
         </div>
-      </InfiniteScroll>
     </div>
   );
 }
